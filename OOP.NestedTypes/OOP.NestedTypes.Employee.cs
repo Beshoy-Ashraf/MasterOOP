40adad25bf34be433ac78c77318c1bc6682759ee
besho <bishoy.ashraf.a046@gmail.com>
2025-06-09 22:06:02 +0300

    Nested Type

--- OOP.NestedTypes/OOP.NestedTypes.Employee.cs
+++ OOP.NestedTypes/OOP.NestedTypes.Employee.cs
@@ -2,6 +2,16 @@ namespace OOP.NestedTypes;
 
 public class Employee
 {
+    public int id{get;set;}
+    public string name{get;set;}
+    public Insurance insurance{get;set;}
     
-    
+    public Employee ()=>insurance =new Insurance{PolicyID = -1 ,CompanyName = "N/A"};
+
+    public class  Insurance
+    {
+        public int PolicyID { get; set; }
+        public String CompanyName { get; set; }
+    }
+
 }
---
+++ OOP.NestedTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP.NestedTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Employee e = new Employee();
+       Console.WriteLine(e.insurance.CompanyName);
+    }
+}

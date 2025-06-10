65496827430941153826fe0e45a80ee3dda5ef48
besho <bishoy.ashraf.a046@gmail.com>
2025-06-10 21:41:51 +0300

    Enum

---
+++ OOP.Enum/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OOP.Enum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine(Month.aug);
+        Console.WriteLine((int)Month.aug);
+        
+        String M = "feb";
+        // Console.WriteLine((int)System.Enum.Parse(typeof(Month), M));
+        if (System.Enum.TryParse(M,out Month month))
+        {
+            Console.WriteLine((int)System.Enum.Parse(typeof(Month),month.ToString()));
+        }
+
+    }
+
+    [Flags]
+    enum Month
+    {
+        jan = 1,
+        feb,
+        mar,
+        apr,
+        may,
+        jun,
+        jul,
+        aug,
+        sep,
+        oct,
+        dec,
+        nov,
+    }
+
+ 
+}
